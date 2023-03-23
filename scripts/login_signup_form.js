00ba940dcec7bf43465cdf1531590a2328203deb
Muzammil <muzammilsiddiqui826@gmail.com>
2023-03-23 23:18:26 +0530

    added switch between signup and login-muzammil

--- scripts/login_signup_form.js
+++ scripts/login_signup_form.js
@@ -1,31 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js"
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js"
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyAxmU8ngD4BQwLOrGz8v3YOfD82vmP2BfY",
-authDomain: "morfeed.firebaseapp.com",
-projectId: "morfeed",
-storageBucket: "morfeed.appspot.com",
-messagingSenderId: "650817798515",
-appId: "1:650817798515:web:04d400054e7cc4c7880506",
-measurementId: "G-DR1VSR1S30"
-};
+    apiKey: "AIzaSyAxmU8ngD4BQwLOrGz8v3YOfD82vmP2BfY",
+    authDomain: "morfeed.firebaseapp.com",
+    databaseURL: "https://morfeed-default-rtdb.firebaseio.com",
+    projectId: "morfeed",
+    storageBucket: "morfeed.appspot.com",
+    messagingSenderId: "650817798515",
+    appId: "1:650817798515:web:04d400054e7cc4c7880506",
+    measurementId: "G-DR1VSR1S30"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
+var currentUser = {};
+
 $("#create-new-user").click(function(){
-    const email = $("#signup_email").val();
-    const password = $("#signup_password").val();
+    var email = $("#signup_email").val();
+    var password = $("#signup_password").val();
     createNewUser(email, password);
     console.log("Sign up Successfull!");
 });
@@ -41,3 +45,28 @@ function createNewUser(email, password){
      console.log(errorMessage);
     });
 }
+
+
+function writeUserData(user) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + user.uid), {
+      email: user.email
+    });
+  }
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      var email = user.email;
+      currentUser = user;
+      writeUserData(user);
+      console.log(currentUser.email + " logged in")
+  
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
