5e74d75b475af7210da16bfe412d41741eda4f3b
Aditya Soni <soniaditya541@gmail.com>
2023-03-23 21:29:35 +0530

    login page submit btn added

---
+++ scripts/login_signup_form.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxmU8ngD4BQwLOrGz8v3YOfD82vmP2BfY",
+    authDomain: "morfeed.firebaseapp.com",
+    projectId: "morfeed",
+    storageBucket: "morfeed.appspot.com",
+    messagingSenderId: "650817798515",
+    appId: "1:650817798515:web:04d400054e7cc4c7880506",
+    measurementId: "G-DR1VSR1S30"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
