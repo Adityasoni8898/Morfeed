29ce8badcc205a990581969f61da05952250966e
Aditya <soniaditya541@gmail.com>
2023-06-16 09:00:35 +0530

    index page mobile bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav_links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+document.querySelector(".nav_links").forEach(n  => n.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+}));
+
+
+
 const btn = document.querySelector('.switch');
 
 btn.addEventListener('click', () => {
